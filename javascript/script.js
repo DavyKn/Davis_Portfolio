b3f3c1f368f0872dc9b203fd36844decedc3be8a
DavyKK <davykk04@gmail.com>
2022-06-14 02:52:54 +0300

    HTML file added and style DOM added with JS

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+const menuMobile = document.getElementById('menu_mobile');
+const hamburgerMenu = document.getElementById('hamburger_menu');
+const closeButton = document.getElementById('close_button');
+const navElement = document.querySelectorAll('.menu_mobile a');
+
+hamburgerMenu.addEventListener('click', () => {
+  menuMobile.style.display = 'flex';
+});
+
+closeButton.addEventListener('click', () => {
+  menuMobile.style.display = 'none';
+});
+
+navElement.forEach((element) => {
+  element.addEventListener('click', () => {
+    menuMobile.style.display = 'none';
+  });
+});
